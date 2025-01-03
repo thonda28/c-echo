1a96e1c5e4ffe7dac1646b1473296253b4079beb
tomoya-honda <tomoya.honda@linecorp.com>
2025-01-03 16:22:56 +0000

    Add error handling for client

--- client.c
+++ client.c
@@ -1,24 +1,45 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
+    // TODO: Refactor redundant error handling
+    int result = 0;
+
     int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        perror("client: socket()");
+        exit(1);
+    }
 
     struct sockaddr_in client_addr;
     client_addr.sin_family = PF_INET;
     client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     client_addr.sin_port = htons(8080);
-    connect(sock, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    result = connect(sock, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    if (result == -1)
+    {
+        perror("client: connect()");
+        close(sock);
+        exit(1);
+    }
 
     char buf[256];
     fgets(buf, sizeof(buf) - 1, stdin);
 
-    send(sock, buf, sizeof(buf), 0);
+    result = send(sock, buf, sizeof(buf), 0);
+    if (result == -1)
+    {
+        perror("client: connect()");
+        close(sock);
+        exit(1);
+    }
 
     close(sock);
 
