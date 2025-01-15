435b1d0be609785e42e1a95d430733c66cba9f3d
tomoya-honda <tomoya.honda@linecorp.com>
2025-01-15 08:51:20 +0000

    Modify client to connect both IPv4 and IPv6 address

--- client.c
+++ client.c
@@ -19,37 +19,70 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    // Initialize the error number
-    errno = 0;
+    // Parse the IP address
+    struct sockaddr_in server_addr4;
+    memset(&server_addr4, 0, sizeof(server_addr4));
+    int is_ipv4 = inet_pton(PF_INET, argv[1], &server_addr4.sin_addr);
+
+    struct sockaddr_in6 server_addr6;
+    memset(&server_addr6, 0, sizeof(server_addr6));
+    int is_ipv6 = inet_pton(PF_INET6, argv[1], &server_addr6.sin6_addr);
+
+    if (is_ipv4 <= 0 && is_ipv6 <= 0)
+    {
+        printf("Invalid IP address: %s\n", argv[1]);
+        exit(1);
+    }
 
     // Parse the port number
     long port = parse_port(argv[2]);
     printf("Port: %ld\n", port);
 
+    // Create a socket and connect to the server
     int sock;
-    if ((sock = socket(PF_INET6, SOCK_STREAM, 0)) == -1)
+    if (is_ipv4 == 1)
     {
-        perror("client: socket()");
+        if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            perror("client: socket()");
+            exit(1);
+        }
+
+        server_addr4.sin_family = PF_INET;
+        server_addr4.sin_port = htons(port);
+
+        if (connect(sock, (struct sockaddr *)&server_addr4, sizeof(server_addr4)) == -1)
+        {
+            perror("client: connect() using IPv4");
+            close(sock);
+            exit(1);
+        }
+    }
+    else if (is_ipv6 == 1)
+    {
+        if ((sock = socket(PF_INET6, SOCK_STREAM, 0)) == -1)
+        {
+            perror("client: socket() using IPv6");
+            exit(1);
+        }
+
+        server_addr6.sin6_family = PF_INET6;
+        server_addr6.sin6_port = htons(port);
+
+        if (connect(sock, (struct sockaddr *)&server_addr6, sizeof(server_addr6)) == -1)
+        {
+            perror("client: connect() using IPv6");
+            close(sock);
+            exit(1);
+        }
+    }
+    else
+    {
+        puts("Reached the unreachable");
         exit(1);
     }
 
-    struct sockaddr_in6 server_addr6;
-    memset(&server_addr6, 0, sizeof(server_addr6));
-    server_addr6.sin6_family = PF_INET6;
-    server_addr6.sin6_port = htons(port);
-    if (inet_pton(PF_INET6, "::1", &server_addr6.sin6_addr) <= 0)
-    {
-        perror("client: inet_pton()");
-        close(sock);
-        exit(1);
-    }
-
-    if (connect(sock, (struct sockaddr *)&server_addr6, sizeof(server_addr6)) == -1)
-    {
-        perror("client: connect()");
-        close(sock);
-        exit(1);
-    }
+    printf("Connected to %s, %ld\n", argv[1], port);
 
     while (1)
     {
